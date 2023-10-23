33140c9e0ca721dd05e123509ad292056c74839b
GiftedR <kidsgamers@hotmail.com>
2023-10-23 15:26:52 -0700

    Added files

---
+++ StringMaker_Staggs/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace StringMaker_Staggs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringManager stringManager = new StringManager();
+            Console.WriteLine(stringManager.Reverse("Sunbeam Tiger"));
+            Console.WriteLine(stringManager);
+            Console.WriteLine(stringManager.Reverse("Sunbeam Tiger", true));
+            Console.WriteLine(stringManager.Equals("Sunbeam Tiger"));
+            Console.WriteLine(stringManager.symmetric("ABBA"));
+            Console.WriteLine(stringManager.symmetric("ABA"));
+            Console.WriteLine(stringManager.symmetric("ABba"));
+        }
+    }
+}
